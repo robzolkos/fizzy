0ba8f2194bf1b3c712e6f18db9d4d7c3e2e70378
Jorge Manrubia <jorge@37signals.com>
2025-07-23 11:45:01 +0200

    Extract methods

--- app/controllers/admin/prompt_sandboxes_controller.rb
+++ app/controllers/admin/prompt_sandboxes_controller.rb
@@ -15,13 +15,10 @@ class Admin::PromptSandboxesController < AdminController
     @llm_model = params[:llm_model] || Event::Summarizer::LLM_MODEL
 
     if @prompt = cookies[:prompt].presence
-      summarizer = Event::Summarizer.new(@day_timeline.events, prompt: @prompt, llm_model: @llm_model)
-      @summary = Event::ActivitySummary.new(content: summarizer.summarize).to_html
-      @summarizable_content == summarizer.summarizable_content.html_safe
+      summarize
       cookies[:prompt] = nil
     else
-      @prompt = Event::Summarizer::PROMPT
-      @summary = @day_timeline.summary&.to_html
+      show_default_prompt
     end
   end
 
@@ -31,4 +28,16 @@ class Admin::PromptSandboxesController < AdminController
     cookies[:prompt] = @prompt
     redirect_to admin_prompt_sandbox_path(day: @day_timeline.day, llm_model: @llm_model)
   end
+
+  private
+    def summarize
+      summarizer = Event::Summarizer.new(@day_timeline.events, prompt: @prompt, llm_model: @llm_model)
+      @summary = Event::ActivitySummary.new(content: summarizer.summarize).to_html
+      @summarizable_content == summarizer.summarizable_content.html_safe
+    end
+
+    def show_default_prompt
+      @prompt = Event::Summarizer::PROMPT
+      @summary = @day_timeline.summary&.to_html
+    end
 end

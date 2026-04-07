24a7a22e8c7d1703cfb395499ce8eb65127e48f7
Rob Zolkos <rob@zolkos.com>
2026-04-07 09:38:04 -0400

    Simplify timezone controller to just head :no_content

--- app/controllers/my/timezones_controller.rb
+++ app/controllers/my/timezones_controller.rb
@@ -1,11 +1,7 @@
 class My::TimezonesController < ApplicationController
   def update
     Current.user.settings.update!(timezone_name: timezone_param)
-
-    respond_to do |format|
-      format.html { head :no_content }
-      format.json { head :no_content }
-    end
+    head :no_content
   end
 
   private
